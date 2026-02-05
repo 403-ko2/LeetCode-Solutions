9a6d2caebc79945190b4d568bb7d044ba6ffe5ff
Zaecorra <177669720+403-ko2@users.noreply.github.com>
2026-02-04 20:33:38 -0500

    Update 169. Majority Element.cpp

--- Two-pointer+Sliding Window/169. Majority Element.cpp
+++ Two-pointer+Sliding Window/169. Majority Element.cpp
@@ -1,3 +1,11 @@
+/*
+three solutions here. 
+first one is a brute force solution utilizing an unordered hashmap (faster than an ordered) to keep track of element count
+second one sorts the array since the majority should be greater than n / 2 so if you return the nums[nums.size() / 2] you would get the element that is going over the half
+last one is the boyer-moore algorithm that increments the count of the result and decrements if the next iteration is not equal to the result. it will reassign result if count is going to be negative.
+
+*/
+
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
@@ -20,12 +28,12 @@ public:
 
         return result;
 
-        // sorting method (O(n log n) time and O(1) or O(n) space depending on the sorting algoritm)
+// sorting method (O(n log n) time and O(1) or O(n) space depending on the sorting algoritm)
 
         // sort(nums.begin(), nums.end());
         // return nums[nums.size() / 2];
 
-        //Boyer-Moore Voting Algorithm
+//Boyer-Moore Voting Algorithm
 
     //     int result = 0, count = 0;
 

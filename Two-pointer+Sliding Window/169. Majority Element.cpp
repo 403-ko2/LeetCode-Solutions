8de4253f7313d494033d8a6f7c2cfff6e9a5ea97
Zaecorra <177669720+403-ko2@users.noreply.github.com>
2026-02-06 23:10:00 -0500

    Update comments for majority element solutions
    Comment out the actual code for the majority element method and uncomment the other solution.

--- Two-pointer+Sliding Window/169. Majority Element.cpp
+++ Two-pointer+Sliding Window/169. Majority Element.cpp
@@ -3,6 +3,9 @@ three solutions here.
 first one is a brute force solution utilizing an unordered hashmap (faster than an ordered) to keep track of element count
 second one sorts the array since the majority should be greater than n / 2 so if you return the nums[nums.size() / 2] you would get the element that is going over the half
 last one is the boyer-moore algorithm that increments the count of the result and decrements if the next iteration is not equal to the result. it will reassign result if count is going to be negative.
+
+just comment out the actaul code after the majority element method and un comment the other solution. 
+
 */
 
 class Solution {
@@ -32,6 +35,7 @@ public:
         // sort(nums.begin(), nums.end());
         // return nums[nums.size() / 2];
 
+        
 //Boyer-Moore Voting Algorithm
 
     //     int result = 0, count = 0;

ff38cc4042ce410460467dec83ea4ca52fc2122b
Zaecorra <177669720+403-ko2@users.noreply.github.com>
2026-02-26 23:35:48 -0500

    Add alternative merge solution for sorted lists
    Added an alternative solution for merging two sorted linked lists.

--- LinkedLists/21.Merge Two Sorted Linked Lists.py
+++ LinkedLists/21.Merge Two Sorted Linked Lists.py
@@ -44,3 +44,15 @@ class Solution:
             tail.next = list2
 
         return dummy.next
+
+#another solution
+"""
+        if not list1 or not list2:
+            return list1 if list1 else list2
+
+        if list1.val > list2.val:
+            list1, list2 = list2, list1
+
+        list1.next = self.mergeTwoLists(list1.next, list2)
+        return list1
+"""

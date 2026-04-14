dc626cd318e291da095e84b0cd8a95927dcb6280
Zaecorra <177669720+403-ko2@users.noreply.github.com>
2026-04-14 08:28:31 -0400

    Update 872. Leaf-Similar Trees.py

--- Depth-First Search/872. Leaf-Similar Trees.py
+++ Depth-First Search/872. Leaf-Similar Trees.py
@@ -1,2 +1,34 @@
-#this is a  DFS solution using an iterative approach rather than recursive. BFS wouldnt work here because the leafs closer to the top would get collected thus putting the array our of order.
+#this is a  DFS solution using an iterative approach rather than recursive. BFS wouldnt work here because the leafs closer 
+#to the top would get collected thus putting the array our of order.
 
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def leafSimilar(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
+        def get_all_leafs(root, array):
+            if not root:
+                return
+            stack = [root]
+            while stack:
+                current = stack.pop()
+                if not current.left and not current.right:
+                    array.append(current.val)
+                if current.left:
+                    stack.append(current.left)
+                if current.right:
+                    stack.append(current.right)
+
+        if not root1 or not root2:
+            return False
+
+        ones_leafs, twos_leafs = [], []
+
+        get_all_leafs(root1, ones_leafs)
+        get_all_leafs(root2, twos_leafs)
+
+        return ones_leafs == twos_leafs

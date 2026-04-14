e85cb1ae4b4dc7c787cd1841d7369dd37f050505
Zaecorra <177669720+403-ko2@users.noreply.github.com>
2026-04-14 08:27:53 -0400

    Create 872. Leaf-Similar Trees.py

---
+++ Depth-First Search/872. Leaf-Similar Trees.py
@@ -0,0 +1,2 @@
+#this is a  DFS solution using an iterative approach rather than recursive. BFS wouldnt work here because the leafs closer to the top would get collected thus putting the array our of order.
+
